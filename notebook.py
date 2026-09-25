a915108bfbb17fb1da4e2b73e77a8667ca65a3d9
Louise Germain <louisegermain04@gmail.com>
2026-09-25 11:01:26 +0200

    perf: change of the way to compute fibonacci and add of test for huge numbers

--- notebook.py
+++ notebook.py
@@ -13,17 +13,27 @@ def _():
 
 
 @app.function
-#definition of the function fibonacci
-#version with no recursive function
+#Code find on the website Stack Overflow 
 
-def fibonacci(n):
-    """ Return the n-th fibonacci number
-        Reminder: the Fibonacci sequence is defined by  F(0) = 0d  F(1) = 1   F(n) = F(n−1) + F(n−2)    for n ≥ 2
-        """
-    a,b = 0,1
-    for i in range(n):  
-        a,b = b,a+b
-    return(a)
+def fibonacci(n: int, f0: int = 0, f1: int = 1) -> int:
+    """
+    Efficiently compute the nth Fibonacci number using matrix exponentiation.
+
+    The tuple (a, b) represents a matrix of the form [[a + b, a], [a, b]].
+    It is initialized to the identity matrix [[1, 0], [0, 1]]. The matrix is
+    squared repeatedly, and multiplied by the matrix [[1, 1], [1, 0]] if the
+    current bit of n is 1.
+
+    Once the loop is done, the produced matrix corresponds to [[1, 1], [1, 0]]
+    to the power of n. Multiplying it by the vector [F(1), F(0)] gives the
+    vector [F(n+1), F(n)]. The Fibonacci number F(n) is then returned.
+    """
+    a, b = 0, 1
+    for bit in f"{n:b}":
+        a, b = (a + 2 * b) * a, a * a + b * b
+        if bit == "1":
+            a, b = a + b, a
+    return a * f1 + b * f0
 
 
 @app.cell
@@ -50,6 +60,18 @@ def _(mo):
 @app.cell
 def _(mo, n_slider):
     mo.md(f"F({n_slider.value}) = {fibonacci(n_slider.value)}")
+    return
+
+
+@app.cell
+def _(pytest):
+    #Aditional unit test for large value
+
+    @pytest.mark.parametrize("n",[1000,10**4,10**5,10**6,10**7])
+
+    def test_fibonacci_huge_numbers(n):
+        assert fibonacci(n) > 0
+
     return
 
 
